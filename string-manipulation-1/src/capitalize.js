0a498b752dd791f1d17682153e4893cd01276a04
Mitchell Lund <Mitchlund922@gmail.com>
2022-06-15 02:17:33 +0000

    Completed String-Manipulation-1.

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,14 @@
+/* exported capitalize */
+
+// define a function called capitalize with a parameter of word
+// create an var called capital with a value of word at index 0 with the ---
+// toUpperCase() property
+// return capital variable
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+
+// define a function called capitalize with a parameter of word
+// return toUpperCase property of word object at index 0 plus the toLowerCase --
+// --property of the slice property at index 1 of the word object
