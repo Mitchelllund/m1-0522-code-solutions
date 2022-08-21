d36a7abe6d4922fb7598de4735150b4a0dc24329
Mitchell Lund <Mitchlund922@gmail.com>
2022-08-21 22:37:47 +0000

    Completed Object-manipulation-2.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,8 +1,9 @@
 /* exported defaults */
 function defaults(target, source) {
   for (var key in source) {
-    if (!target.hasOwn(key)) {
+    if (target[key] === undefined) {
       target[key] = source[key];
     }
   }
+  return target;
 }
